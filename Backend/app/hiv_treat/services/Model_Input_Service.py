d6453924fd25f5781012d2089bf3c83dbbe5217b
Vignesh-9602 <vigneshwaran.r@mathco.com>
2026-07-23 13:04:22 +0530

    updated model input service file

--- Backend/app/hiv_treat/services/Model_Input_Service.py
+++ Backend/app/hiv_treat/services/Model_Input_Service.py
@@ -1,4 +1,4 @@
-import pandas as pd
+​import pandas as pd
 from collections import OrderedDict
 from app.db.connection import get_connection
 
@@ -649,7 +649,6 @@ def build_factors(cur, ta, scenario):
             "Forecast",
         ),
     }
- 
 
 
 # =========================================================
